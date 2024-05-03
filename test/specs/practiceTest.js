b8ca490eb6543fd6713f84f4e96789d036b5db25
qafellas <team@qafellas.com>
2024-05-02 22:50:02 -0400

    more ui actions

--- test/specs/practiceTest.js
+++ test/specs/practiceTest.js
@@ -4,10 +4,16 @@ import path from 'path'
 const __dirname = path.resolve()
 
 describe('UI Actions', ()=>{
-    it('should navigate to website', async()=>{
+    beforeEach('setUp', async()=>{
         await browser.url('https://testautomationpractice.blogspot.com/')//Navigate to website
-        await browser.pause(5000)//stop for 5s
+        await browser.pause(1000)//stop for 5s
+    })
 
+    afterEach('tearDown', async()=>{
+        console.log('after each hook...')
+    })
+
+    it('should navigate to website', async()=>{
         const title = await browser.getTitle()// get web page's title
         console.log(title)
         expect(title).to.equal('Automation Testing Practice')
@@ -18,9 +24,6 @@ describe('UI Actions', ()=>{
     })
 
     it('should enter text in a text box', async()=>{
-        await browser.url('https://testautomationpractice.blogspot.com/')
-        await browser.pause(5000)
-
         //Enter text into text box. if there is a previous value in the box, setValue deletes it and enter the new value
         await $('//input[@id="name"]').setValue("Webdriverio is so cooool!") 
         await browser.pause(5000)
@@ -31,17 +34,11 @@ describe('UI Actions', ()=>{
     })
 
     it('should click on web element', async()=> {
-        await browser.url('https://testautomationpractice.blogspot.com/')
-        await browser.pause(3000)
-
         await $('//button[text()="New Browser Window"]').click()
         await browser.pause(3000)
     })
 
     it('should click on multyiple options by using web elements', async()=>{
-        await browser.url('https://testautomationpractice.blogspot.com/')
-        await browser.pause(3000)
-        
         // $$ is used for web elemets to store
         const checkBoxes = await $$('//label[text()="Days:"]//..//input[@type="checkbox"]')
         console.log(checkBoxes.length)
@@ -54,9 +51,6 @@ describe('UI Actions', ()=>{
     })
 
     it('should select option on dropdown menu', async()=> {
-        await browser.url('https://testautomationpractice.blogspot.com/')
-        await browser.pause(3000)
-        
         const countryDropDown = await $('select[id="country"]')
 
         await countryDropDown.selectByVisibleText('Germany')//Germany
@@ -71,9 +65,6 @@ describe('UI Actions', ()=>{
     })
 
     it('should handle alerts', async()=> {
-        await browser.url('https://testautomationpractice.blogspot.com/')
-        await browser.pause(3000)
-
         await $('//button[text()="Confirm Box"]').click()
         await browser.pause(2000)
 
@@ -95,23 +86,14 @@ describe('UI Actions', ()=>{
 
         await browser.dismissAlert()
         await browser.pause(2000)
-
-
-
     })
 
     it('should double-click on the webelement', async()=>{
-        await browser.url('https://testautomationpractice.blogspot.com/')
-        await browser.pause(3000)
-
         await $('//button[text()="Copy Text"]').doubleClick()
         await browser.pause(3000)
     })
 
     it('should drag and drop web elemet', async()=>{
-        await browser.url('https://testautomationpractice.blogspot.com/')
-        await browser.pause(3000)
-
         await $('#draggable').dragAndDrop(await $('#droppable'))
         await browser.pause(3000)
 
@@ -133,5 +115,65 @@ describe('UI Actions', ()=>{
        await browser.pause(2000)
     })
 
+    it('should handle new browser tab', async()=>{
+        await $('//button[text()="New Browser Window"]').click()
+        await browser.pause(2000)
+
+
+        const windowTabs = await browser.getWindowHandles()
+        console.log(windowTabs)
+        
+        // We swicth to the second window
+        await browser.switchToWindow(windowTabs[1])
+        await browser.pause(2000)
+
+        console.log(await browser.getTitle())
+
+        await $('input[placeholder="Search"]').addValue("Iphone")
+        await browser.pause(2000)
+
+        const mp3PlayerMenu = await $('//a[text()="MP3 Players"]')
+        await mp3PlayerMenu.moveTo()
+        await browser.pause(2000)
+
+        await browser.switchToWindow(windowTabs[0])
+        console.log(await browser.getTitle())
+
+    })
+
+    it('should wait until element displayed for a web element', async()=>{
+        await $('#textarea').waitForDisplayed({timeout: 10000})
+        await $('#textarea').setValue('123 Main Street')
+
+        //waitUntil
+
+        await browser.pause(3000)
+    })
+
+    it('should wait until specific condition to be met', async()=>{
+        await $('//button[text()="New Browser Window"]').click()
+        const windowTabs = await browser.getWindowHandles()
+        console.log(windowTabs)
+        
+        // We swicth to the second window
+        await browser.switchToWindow(windowTabs[1])
+
+        await browser.waitUntil(async function () {
+            return (await $('//img[@alt="MacBookAir"]//..//..//..').getAttribute('class')) === 'carousel-item active'//condition
+          }, {
+            timeout: 5000,//ms
+            timeoutMsg: 'expected attribute value to be different after 5s'
+          })
+
+        
+    })
+
+    it('should get text of web element', async()=> {
+        const text = await $('//button[text()="Copy Text"]//following-sibling::p').getText()
+        expect(text).to.equal('Double click on button, the text from Field1 will be copied into Field2.')
+
+        await browser.pause(3000)
+
+    })
 
 })
